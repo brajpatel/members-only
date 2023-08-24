ec061efb7777823bd81577bf883d47d706e27af1
brajpatel <brajeshpatel0002@gmail.com>
2023-08-24 13:54:54 +0100

    Create route to all messages and message detail

--- routes/messages.js
+++ routes/messages.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("messages", { title: "All messages" });
+  res.render("message_list", { title: "All messages" });
+});
+
+router.get('/:id', function(req, res, next) {
+    res.render("message_detail", { title: "Message Detail" })
 });
 
 module.exports = router;
