59511dc0c6fbae04aaf0df1775003a95af82db1f
brajpatel <brajeshpatel0002@gmail.com>
2023-08-25 10:49:56 +0100

    Create login and sign up routes

---
+++ routes/account.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render("login_form", { title: "Login" });
+});
+
+router.get('/sign-up', function(req, res, next) {
+    res.render("signup_form", { title: "Sign in" });
+  });
+
+module.exports = router;
