ea1727f1f9e3aa1ddc498674143d4ebc364c0241
brajpatel <brajeshpatel0002@gmail.com>
2023-08-25 11:48:29 +0100

    Change url for becoming a member and admin

--- routes/account_status.js
+++ routes/account_status.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/become-a-member', function(req, res, next) {
-    res.render("become_member", { title: "Become a member" });
+    res.render("become_member", { title: "Become a member", user: { account_status: "none" } });
 })
 
-router.get('/become-an-member', function(req, res, next) {
-    res.render("become_admin", { title: "Become an admin" });
+router.get('/become-an-admin', function(req, res, next) {
+    res.render("become_admin", { title: "Become an admin", user: { account_status: "member" } });
 })
 
 module.exports = router;
