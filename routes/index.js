93e8ace9d9d25e9e99e428be11b66992991eaa75
brajpatel <brajeshpatel0002@gmail.com>
2023-08-28 10:30:58 +0100

    Get user object

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("message_list", { user: req.user });
+  res.render("message_list", {
+    ...(req.user) && { user: req.user },
+    ...(!req.user) && { user: undefined }
+  });
 });
 
 module.exports = router;
