0a819b414435fd80822be1dd8393db351c9adce8
brajpatel <brajeshpatel0002@gmail.com>
2023-08-31 11:49:09 +0100

    Create message post route controller to allow messages to be created

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,11 +1,11 @@
+const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 const Message = require('../models/message');
 
 exports.message_list = asyncHandler(async (req, res, next) => {
-    const messages = await Message.find().exec();
+    const allMessages = await Message.find().populate("author").exec();
 
-    res.render('message_list', { title: "All Messages", message_list: messages });
-
+    res.render('message_list', { title: "All Messages", message_list: allMessages });
 })
 
 exports.message_create_get = asyncHandler(async (req, res, next) => {
@@ -16,7 +16,36 @@ exports.message_create_get = asyncHandler(async (req, res, next) => {
     res.render("message_form", { title: "Create new message", user: req.user });
 });
 
-exports.message_create_post = []
+exports.message_create_post = [
+    body("message", "Message cannot be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const message = new Message({
+            text: req.body.message,
+            date_added: new Date().toLocaleDateString(),
+            author: req.body.author
+        })
+
+        if(!errors.isEmpty()) {
+            res.render("message_form", {
+                title: "Create new message",
+                user: req.user,
+                errors: errors.array()
+            })
+
+            return;
+        }
+        else {
+            await message.save();
+            res.redirect("/");
+        }
+    })
+]
 
 exports.message_delete_get = []
 
--- models/message.js
+++ models/message.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
     text: { type: String, required: true },
-    date_added: { type: Date },
+    date_added: { type: String, required: true },
     author: { type: Schema.Types.ObjectId, ref: "User" }
 })
 
