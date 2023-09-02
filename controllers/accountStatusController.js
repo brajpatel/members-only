d6a8c32c5d6193545a911482bae02e70e23351de
brajpatel <brajeshpatel0002@gmail.com>
2023-09-02 11:32:16 +0100

    Make a user a member if they get the password correct

--- controllers/accountStatusController.js
+++ controllers/accountStatusController.js
@@ -12,7 +12,13 @@ exports.become_member_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.become_member_post = asyncHandler(async (req, res, user) => {
-    
+    if(req.body.member_password === process.env.MEMBER_PASSWORD) {
+        await User.findByIdAndUpdate(req.user._id, { account_status: "member" }, {});
+        res.redirect('/');
+    }
+    else {
+        res.render("become_member", { title: "Try Again!", user: req.user });
+    }
 })
 
 // member_password*1*2*3_____x
